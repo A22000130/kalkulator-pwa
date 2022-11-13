6e20f8661465bfd79d4a1bd912ffd478a9b7a834
A22000130 <a2.2000130@mhs.stmik-sumedang.ac.id>
2022-11-13 17:01:56 +0700

    commit 3

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'SW-001';
+const CACHE_NAME = 'SW-002';
 const toCache = [
   '/',
-  'manifest.json',
-  'assets/js/register.js',
-  'assets/images/calculator.png',
+  '/manifest.webmanifest',
+  '/assets/js/register.js',
+  '/assets/images/icon-192x192.png',
 ];
 self.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
@@ -14,36 +14,36 @@ self.addEventListener("beforeinstallprompt", (e) => {
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then(function (cache) {
-      return cache.addAll(toCache)
-    })
-    .then(self.skipWaiting())
+      .then(function (cache) {
+        return cache.addAll(toCache)
+      })
+      .then(self.skipWaiting())
   )
 })
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     fetch(event.request)
-    .catch(() => {
-      return caches.open(CACHE_NAME)
-        .then((cache) => {
-          return cache.match(event.request)
-        })
-    })
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match(event.request)
+          })
+      })
   )
 })
 self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.keys()
-    .then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Hapus cache lama',
-            key)
-          return caches.delete(key)
-        }
-      }))
-    })
-    .then(() => self.clients.claim())
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Hapus cache lama',
+              key)
+            return caches.delete(key)
+          }
+        }))
+      })
+      .then(() => self.clients.claim())
   )
 })
 // const cacheName = 'SW-001';
